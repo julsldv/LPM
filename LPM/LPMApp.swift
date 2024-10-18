beef51ea356bef7a8ca2a96aec7741431e5bf6ef
julsldv <julsldv@gmail.com>
2024-10-18 17:32:26 +0200

    Initial Commit

---
+++ LPM/LPMApp.swift
@@ -0,0 +1,17 @@
+//
+//  LPMApp.swift
+//  LPM
+//
+//  Created by Julien PORTOLAN on 18/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct LPMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
